d0faac540c4c1881451b84bd426d6bf126b620a4
hatim85 <devconnect.hatim@gmail.com>
2024-07-03 10:23:54 +0530

    Added logout functionality

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import MovieItem from './MovieItem';
 import { setMoviesSuccess } from '../redux/moviesSlice';
+import { signout } from '../redux/authSlice';
 import '../../styles/MovieList.css'; 
 
 const MovieList = () => {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const movies = useSelector((state) => state.movies.movies);
 
     useEffect(() => {
@@ -16,10 +19,18 @@ const MovieList = () => {
             });
     }, [dispatch]);
 
+    const handleLogout = () => {
+        dispatch(signout());
+        navigate('/signin');
+    };
+
     return (
         <div className="movie-list-container">
             <h1 className="movie-list-title">Movie Watchlist</h1>
-            <button className="btn add-movie-btn" onClick={() => window.location.href = '/addmovie'}>Add Movie</button>
+            <div className="movie-list-actions">
+                <button className="btn add-movie-btn" onClick={() => window.location.href = '/addmovie'}>Add Movie</button>
+                <button className="btn logout-btn" onClick={handleLogout}>Logout</button>
+            </div>
             <ul className="movie-list">
                 {movies.map((movie) => (
                     <MovieItem key={movie._id} movie={movie} />
